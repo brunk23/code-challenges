f5096ba28222c2fad1515231c9caa0336f2b483b
Kevin Brunelle <brunellek@gmail.com>
2015-12-06 01:10:16 -0500

    Commended added to part2 of day 6

--- AdventOfCode/day06-part2.cpp
+++ AdventOfCode/day06-part2.cpp
@@ -1,4 +1,4 @@
-// Day 6 part 1
+// Day 6 part 2
 #include <iostream>
 #include <string>
 
@@ -7,16 +7,19 @@ using std::cin;
 using std::endl;
 using std::string;
 using std::getline;
-using std::size_t;
 
 #define LIGHTS 1000
 
 int main()
 {
+  // We have a grid of 1000 x 1000 lights (problem statement)
   char grid[LIGHTS][LIGHTS];
-  int pos[4];
-  int point = 0;
-  bool ondig = false;
+  int pos[4];			// x1,y1 and x2,y2
+  int point = 0;		// which of the above we are finding
+  bool ondig = false;		// are we on a number or not?
+
+  // this reeks of "magic numbers" 1 = on, 2 = off, 3 = toggle
+  // and is a dirty method. an enum would be better in most code
   int operation = 0;
   
   for (int x = 0; x < LIGHTS; x++) {
@@ -27,12 +30,19 @@ int main()
   
   string input;
   while (getline(cin, input) ) {
+
+    // Reset the variables we are using to decode the input
     operation = 0;
     point = 0;
     ondig = false;
     for (int x = 0; x < 4; x++ ) {
       pos[x] = 0;
     }
+
+    // Determine what we're going to be doing with these
+    // two points. These are specific to the problem statement
+    // and are fixed in the input file (we trust that the case
+    // and format won't be mangled.
     if ( input.find("turn on") != string::npos ) {
       operation = 1;
     }
@@ -43,10 +53,19 @@ int main()
       operation = 3;
     }
     if ( operation == 0 ) {
-      cout << "Invalid input.!" << endl;
+      // This shouldn't happen with the given input file
+      // if it does, we ignore this line and go to the next
+      cout << "Invalid input!" << endl;
       continue;
     }
+
+    // Process the string looking for the 4 numbers that
+    // are the points of the two corners of each rectangle
     for (unsigned int x = 0; x < input.size(); x++) {
+
+      // First we handle digits we make a note that we were
+      // on a digit already so we know if we need to go to the
+      // next value in the array.
       switch ( input[x] ) {
       case '0':
       case '1':
@@ -63,39 +82,49 @@ int main()
 	pos[point] += input[x] - '0';
 	break;
       case ',':
-	point++;
+	point++;		// A , separates two values
 	break;
       default:
-	if ( ondig ) {
-	  point++;
+	if ( ondig ) {		// If we left digits, the next
+	  point++;		// digit will be a new value
 	  ondig = false;
 	}
       }
     }
+
+    // We now have the two points let's actually turn the
+    // lights up or down as directed.
     for (int x = pos[0]; x <= pos[2]; x++) {
       for (int y = pos[1]; y <= pos[3]; y++) {
 	switch (operation) {
 	case 1:
+	  // case 1 (turn on) increase by 1
 	  grid[x][y]++;
 	  break;
 	case 2:
+	  // case 2 (turn off) decrease by 1, unless 0
 	  if ( grid[x][y] )
 	    grid[x][y]--;
 	  break;
 	default:
-	  grid[x][y]++;
-	  grid[x][y]++;
+	  // case 3 (toggle) increase by 2
+	  grid[x][y]+=2;
 	}
       }
     }
   }
-  
+
+  // We use point here to save the total brightness
   point = 0;
+
+  // get the sum of all the values in the array.
   for (int x = 0; x < LIGHTS; x++ ) {
     for (int y = 0; y < LIGHTS; y++ ) {
       point += grid[x][y];
     }
   }
+
+  // Tell the user what it was.
   cout << "The total brightness is " << point << " !" << endl;
   
   return 0;
