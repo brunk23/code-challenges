a42f668af4ddfcf205bbff2dc180f2c5e6537d1e
Kevin Brunelle <brunellek@gmail.com>
2015-12-09 11:24:06 -0500

    Set up data structures

--- AdventOfCode/day09-part1.cpp
+++ AdventOfCode/day09-part1.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include <string>
-#include <stringstream>
 
 using std::cout;
 using std::cin;
 using std::endl;
 using std::getline;
+using std::string;
+
+const int CITIES = 10;
 
 int main()
 {
+	string names[CITIES];
+	int distance[CITIES][CITIES];
+	
+	for(int x = 0; x < CITIES; x++) {
+		for(int y = 0; y < CITIES; y++) {
+			distance[x][y] = 0;
+		}
+		names[x] = "";
+	}
 
 
 	return 0;
