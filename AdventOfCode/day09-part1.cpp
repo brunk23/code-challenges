e40a1b763ce4c43477ba35415138a88cd2f854b9
Kevin Brunelle <brunellek@gmail.com>
2015-12-09 12:17:02 -0500

    Start parsing input

--- AdventOfCode/day09-part1.cpp
+++ AdventOfCode/day09-part1.cpp
@@ -6,8 +6,11 @@ using std::cin;
 using std::endl;
 using std::getline;
 using std::string;
+using std::size_t;
 
 const int CITIES = 10;
+
+void read_input(string names[CITIES], int distance[CITIES][CITIES]);
 
 int main()
 {
@@ -21,6 +24,34 @@ int main()
 		names[x] = "";
 	}
 
+	read_input(names, distance);
+
 
 	return 0;
+}
+
+enum TOKEN {
+	FIRST,
+	SECOND
+};
+
+void read_input(string names[CITIES], int distance[CITIES][CITIES]) {
+	string input, name1, name2;
+	TOKEN location = FIRST;
+	size_t start, len;
+
+	while( getline(cin, input) ) {
+		start = 0;
+		len = input.find(" to ");
+		name1 = input.substr(start,len);
+		start += len + 4;
+		len = input.find(" = ");
+		len -= start;
+		name2 = input.substr(start,len);
+		start += len + 4;
+		cout << input << endl;
+		cout << "name1: " << name1 << endl 
+		     << "name2: " << name2 << endl;
+	}
+
 }
