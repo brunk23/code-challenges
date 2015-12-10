572074dd16694ec92d4c4a0878c076e310c1271c
Kevin Brunelle <brunellek@gmail.com>
2015-12-10 09:39:35 -0500

    Remove costly string operations due to limited character possibilities, remove costly string processing and stringstream over-riding

--- AdventOfCode/day10-part2.cpp
+++ AdventOfCode/day10-part2.cpp
@@ -8,47 +8,63 @@ using std::cout;
 using std::cin;
 using std::endl;
 using std::string;
-using std::stringstream;
 
 int main()
 {
-  int count = 0;
-  stringstream number;
-  string input = "1321131112";
-  string next = "";
-  char current = '\0';
-  
-  for (int i = 0; i < 50; ++i ) {
-    next = "";    
-    count = 0;
-    for ( unsigned int x = 0; x < input.size(); x++) {
+	int count = 0;
+	string input = "1321131112";
+	string next = "";
+	char count_c;
+	char current = '\0';
+	
+	for (int i = 0; i < 50; ++i ) {
+		next = "";	
+		count = 0;
+		for ( unsigned int x = 0; x < input.size(); x++) {
 
-	if( current == input[x] ) {
-		count++;
-	} else {
-		if( current && count ) {
-			number.str("");
-			number << count;
-			next = next + number.str();
-			next = next + current;
+			if( current == input[x] ) {
+				count++;
+			} else {
+				if( current && count ) {
+					// We can tighten this up because count is
+					// limited to 1, 2, or 3 according to the wikipedia
+					// pages on the Look-and-say sequence
+					if(count == 1) {
+						count_c = '1';
+					} else {
+					if(count == 2) {
+						count_c = '2';
+					} else {
+					if(count == 3) {
+						count_c = '3';
+					} else {
+						cout << "ERROR";
+					}}}	 // Terminate the nested if blocks
+					next = next + count_c + current;
+				}
+				current = input[x];
+				count = 1;
+			}
 		}
-		current = input[x];
-		count = 1;
+		if(count == 1) {
+			count_c = '1';
+		} else {
+		if(count == 2) {
+			count_c = '2';
+		} else {
+		if(count == 3) {
+			count_c = '3';
+		} else {
+			cout << "ERROR";
+		}}}	 // Terminate the nested if blocks
+		next = next + count_c + current;
+		input = next;	
+			// cout << i << ": " << input << endl;
+		cout << i << ": " << input.size() << endl;
 	}
-    }
-    number.str("");
-    number << count;
-    next = next + number.str();
-    number.str("");
-    number << current;
-    next = next + number.str();
-    input = next;	
-   	// cout << i << ": " << input << endl;
-    cout << i << ": " << input.size() << endl;
-  }
-  
-    cout << next.size() << endl;
+	
+	cout << next.size() << endl;
 
 
-  return 0;
+	return 0;
 }
