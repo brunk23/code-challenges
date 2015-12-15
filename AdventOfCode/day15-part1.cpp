ec379c50e1d01095487fa2b50541896c03f6ec71
Kevin Brunelle <brunellek@gmail.com>
2015-12-15 12:45:34 -0500

    Part 1, hard-coded

--- AdventOfCode/day15-part1.cpp
+++ AdventOfCode/day15-part1.cpp
@@ -10,34 +10,56 @@ using std::string;
 // we don't know how big of a number we might need
 typedef long score;
 
-score mix_score(int spr, int but, int cho, int can);
+score mix_score(int spr, int but, int cho, int can, int values[4][5]);
 
 int main()
 {
 	// There are 4 ingredients and 5 qualities
-	int values[4][5];
-	int amount[4];
-	int spr, but, cho, can;
-	score max, temp;;
+	// Hard coded to speed things up
+	int values[4][5] = {	{2, 0, -2, 0, 3},
+				{0, 5, -3, 0, 3},
+				{0, 0, 5, -1, 8},
+				{0, -1, 0, 5, 8}};
+	// int amount[4];
+	int spr, but, cho;
+	score max=0, temp=0;
 
 	for( spr = 0; spr <= 100; ++spr ) {
 	for( but = 0; but <= (100-spr); ++but ) {
 	for( cho = 0; cho <= (100-spr-but); ++cho ) {
-		temp = mix_score(spr, but, cho, (100-spr-but-cho));
+		temp = mix_score(spr, but, cho, (100-spr-but-cho), values);
 		if ( temp > max ) {
 			max = temp;
 		}
 	}}}	// end the for loops
 
+	cout << "The max score was: " << max << endl;
 
 	return 0;
 }
 
-score mix_score(int spr, int but, int cho, int can)
+score mix_score(int spr, int but, int cho, int can, int values[4][5])
 {
 	int cap, dur, fla, tex, cal;
+	score product;
 	
-	cout << spr << " " << but << " " << cho << " " << can << endl;
+	cap = values[0][0] * spr + values[1][0] * but + values[2][0] * cho
+		+ values[3][0] * can;
+	dur = values[0][1] * spr + values[1][1] * but + values[2][1] * cho
+		+ values[3][1] * can;
+	fla = values[0][2] * spr + values[1][2] * but + values[2][2] * cho
+		+ values[3][2] * can;
+	tex = values[0][3] * spr + values[1][3] * but + values[2][3] * cho
+		+ values[3][3] * can;
+	if( cap < 0 )
+		cap = 0;
+	if( dur < 0)
+		dur = 0;
+	if( fla < 0 )
+		fla = 0;
+	if( tex < 0 )
+		tex = 0;
+	product = cap * dur * fla * tex;
 
-	return 0;
+	return product;
 }
