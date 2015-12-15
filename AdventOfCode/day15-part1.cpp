c9bb378177192a0da342fa30a6bb8012324d547b
Kevin Brunelle <brunellek@gmail.com>
2015-12-15 10:50:44 -0500

    Rework the main program

--- AdventOfCode/day15-part1.cpp
+++ AdventOfCode/day15-part1.cpp
@@ -10,16 +10,25 @@ using std::string;
 // we don't know how big of a number we might need
 typedef long score;
 
-score mix_score(int itemcount, int *values, int *amount);
+score mix_score(int spr, int but, int cho, int can, int *amount);
 
 int main()
 {
-	// we will use two arrays
-	// amount [   xx, xx, xx, xx, ...] how much of each ingredient
-	// values [[  ca, ca, ca, ca, ...]
-	//	   [  du, du, du, du, ...]
-	//	   [  fl, fl, fl, fl, ...] etc.]
-	// Then I can multiply amount[x] * values[n][x] 
+	// There are 4 ingredients and 5 qualities
+	int values[4][5];
+	int amount[4];
+	int spr, but, cho, can;
+	score max, temp;;
+
+	for( spr = 0; spr <= 100; ++spr ) {
+	for( but = 0; but <= 100; ++but ) {
+	for( cho = 0; cho <= 100; ++cho ) {
+	for( can = 0; can <= 100; ++can ) {
+		temp = mix_score(spr, but, cho, can, values);
+		if ( temp > mix_score ) {
+			mix_score = temp;
+		}
+	}}}}	// end the for loops
 
 
 	return 0;
