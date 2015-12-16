cf13d99a669febc97b22e41ab8343e24a8fa53ec
Kevin Brunelle <brunellek@gmail.com>
2015-12-16 07:07:03 -0500

    Ugly, but it works This probably isn't the most elegant solution, but it parses the input one key at a time, and continues to the next line if a qualification fails. Prints any and all matches (which there is only 1 for both)

---
+++ AdventOfCode/day16-part1.cpp
@@ -0,0 +1,184 @@
+#include <iostream>
+#include <string>
+
+using std::getline;
+using std::cin;
+using std::cout;
+using std::string;
+using std::endl;
+
+int main()
+{
+	size_t location;
+	int value = 0;
+	string input;
+	bool possible = true;
+
+	while( getline(cin, input) ) {
+		if( (location = input.find("children: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 3 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("cats: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 7 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("samoyeds: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 2 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("pomeranians: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 3 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("akitas: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 0 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("vizslas: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 0 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("goldfish: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 5 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("trees: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 3 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("cars: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 2 ) {
+				continue;
+			}
+		}
+
+		if( (location = input.find("perfumes: ")) != std::string::npos) {
+			value = 0;
+			for(unsigned int x = location; x < input.size(); ++x) {
+				if(input[x] == ',') {
+					break;
+				}
+				if(input[x] >= '0' && input[x] <= '9') {
+					value *= 10;
+					value += input[x] - '0';
+				}
+			}
+			if( value != 1 ) {
+				continue;
+			}
+		}
+
+		// This Sue could qualify
+		cout << input << endl;
+
+	}
+
+	return 0;
+}
