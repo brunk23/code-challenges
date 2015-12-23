dab2c02c009eb8c7f32d459d062fbbe132da040a
Kevin Brunelle <brunellek@gmail.com>
2015-12-22 22:31:07 -0500

    Refactoring Making the program into component parts

---
+++ src/shared/sml_shared.h
@@ -0,0 +1,22 @@
+#ifndef SML_SHARED_H
+#define SML_SHARED_H
+
+#define MEMSIZE 100
+#define MAXOP 100
+#define MAXVAL 9999
+#define MINVAL -9999
+
+/*
+ * Not all these opcodes were part of the original spec.
+ *   Extended Opcodes Include: INC, DEC, MOD, DUMP, NOP
+ */
+enum OPCODES {
+	READ = 10, WRITE,
+	LOAD = 20, STORE,
+	ADD = 30, SUBTRACT, DIVIDE, MULTIPLY, MOD,
+	BRANCH = 40, BRANCHNEG, BRANCHZERO, HALT,
+	INC = 60, DEC,
+	NOP = 98, DUMP
+};
+
+#endif
