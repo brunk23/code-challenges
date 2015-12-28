2850df27b6867709424ad7722517d35ac472ae73
Kevin Brunelle <brunellek@gmail.com>
2015-12-27 19:55:30 -0500

    Going to hand write my string parser here

--- SML/src/lispcomp/compile_io.c
+++ SML/src/lispcomp/compile_io.c
@@ -1,5 +1,12 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
 #include "compiler.h"
 #include "compile_io.h"
+#include "compile_utility.h"
+#include "compile_messages.h"
+#include "compile_function.h"
 
 /* Use Empty Place Holder for testing purposes */
 int process_source(char *filename, int core[]) {
@@ -10,28 +17,62 @@ int process_source(char *filename, int core[]) {
   ssize_t status;
   
   int retcode = 0;
-  int x, dest, strtmp, strindex;
+  int x, y, dest, strtmp, strindex;
 
   struct Token *base;
+
+  /*
+   * Open the source file.  Fail if we can't.
+   */
+  if( !( source = fopen(filename, "r")) ) {
+    currline(filename,strlen(filename)+1,-1);
+    emessg("Could not open source file", 1);
+  }
 
   /*
    * Create the base node that our program will be under.
    * This is of type "PROGN" and will have an ID of 1.
    */
   base = malloc(sizeof(struct Token));
-  base->symbolTree = 0;
+  base->symTree = 0;
   base->next = 0;
   base->arg = 0;
   base->type = PROGN;
   base->ID = getID();
   base->location = 0;
+
+  /*
+   * This is the first pass of the compiler.
+   */
+  while(( status = getline(&line, &bytes_read, source)) != -1) {
+    linenumber++;
+    curr = strtok(line, "\n");
+    
+    for( x = 0; x < strlen(line); ++x ) {
+      if(curr[x] == '(') {
+	y = x+1;
+	while( curr[x] != ' ' && curr[x] != 0 ) {
+	  ++x;
+	}
+	curr[x] = 0;
+	process_functions(&curr[y],base);
+	curr[x] = '|';
+      }
+
+    }
+  }
   
 
-  if( !( source = fopen(filename, "r")) ) {
-    currline(filename,strlen(filename)+1,-1);
-    emessg("Could not open source file", 1);
-  }
 
+
+
+  /*
+   * Before we actually compile the parsed tree
+   * Zero out the core memory
+   */
+  for( x = 0; x < MEMSIZE; ++x ) {
+    core[x] = 0;
+  }
 
   return 0;
 }
