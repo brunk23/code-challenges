79260024d1ce5eb0f7fe4b06c34ac1d614833c22
Kevin Brunelle <brunellek@gmail.com>
2015-12-30 01:22:53 -0500

    Inlines and flags

--- AdventOfCode/Day25/day25-part1.cpp
+++ AdventOfCode/Day25/day25-part1.cpp
@@ -3,6 +3,10 @@
 using std::cout;
 using std::endl;
 
+// Original:           0m00.60s real     0m00.56s user     0m00.00s system
+// With inline -O2:    0m00.25s real     0m00.22s user     0m00.00s system
+
+
 long triangle_number(long n);
 int which_number(int row, int col);
 long generate_code(long x);
@@ -28,7 +32,7 @@ int main()
   return 0;
 }
 
-long generate_code(long inp)
+inline long generate_code(long inp)
 {
   long temp;
   temp = inp * 252533;
@@ -36,14 +40,14 @@ long generate_code(long inp)
   return temp;
 }
 
-int which_number(int row, int col)
+inline int which_number(int row, int col)
 {
   return ( triangle_number(col) + triangle_number(col + row - 2)
 	  - triangle_number(col - 1) );
 }
 
 // These are triangular numbers.
-long triangle_number(long n)
+inline long triangle_number(long n)
 {
   return (n*(n+1)/2);
 }
