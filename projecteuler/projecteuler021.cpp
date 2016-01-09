74e028def50bd5f997896429d93c538350971010
Kevin Brunelle <brunellek@gmail.com>
2016-01-09 16:25:28 -0500

    Problem 21 was pretty simple Especially when you write d() as described. It may not be the most efficient solution, but it returns the result in about 1.3 seconds

---
+++ projecteuler/projecteuler021.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+int d(int);
+
+int main()
+{
+  int x = 0, sum = 0;
+
+  for( x = 2; x <= 10000; ++x ) {
+    if( x == d(d(x)) && x != d(x) ) {
+      sum += x;
+    }
+  }
+
+  cout << "Total found: " << sum << endl;
+  
+  return 0;
+}
+
+
+/*
+ * Will return the sum of all factors of a number
+ * d(24) = 1 + 2 + 3 + 4 + 6 + 8 + 12 == 36
+ */
+int d(int numb) {
+  int sum = 0, x = 0;
+
+  for( x = 1; x < numb; ++x) {
+    if( numb%x == 0 ) {
+      sum += x;
+    }
+  }
+
+  return sum;
+}
+  
