f7ed245ba7e129c2c5baa0f457ec3c09bcd5c3d7
Kevin Brunelle <brunellek@gmail.com>
2016-02-10 18:54:16 -0500

    Just a simple in/out test

--- logger/main.c
+++ logger/main.c
@@ -1,11 +1,28 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 /*
  * Will be a program which logs various information
  */
 
 int main(int argc, char *argv[]) {
+  char *str = 0;
+  size_t linesize = 0;
+  ssize_t linelen;
 
+  printf("What is your name? ");
+  linelen = getline(&str, &linesize, stdin);
+  if(linelen == -1 ) {
+    fprintf(stderr,"No input\n");
+    exit(1);
+  }
+  str = strtok(str,"\n");
+  printf("Hello %s\n",str);
+
+  if(str) {
+    free(str);
+  }
 
   return 0;
 }
