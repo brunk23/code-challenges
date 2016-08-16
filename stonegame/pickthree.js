00b6a76fc09531fcb222c03791b269f518723665
Kevin Brunelle <brunellek@gmail.com>
2016-08-16 17:28:16 -0400

    Add the function for the computer player.

--- stonegame/pickthree.js
+++ stonegame/pickthree.js
@@ -3,7 +3,7 @@ var highest = 18;
 var stones;
 
 /*
- * A random number between what.
+ * A random number between two values inclusive.
  */
 function randBetween(low, high) {
     var a, b;
@@ -18,7 +18,7 @@ function randBetween(low, high) {
 }
 
 /*
- * Pick three starting numbers
+ * Pick three starting numbers without any duplicates
  */
 function pickthree(low,high) {
     var a = [];
@@ -53,6 +53,9 @@ function pickthree(low,high) {
     return a;
 }
 
+/*
+ * This function will display the stone counts.
+ */
 function displayStones() {
     document.getElementById("box1").innerHTML = '<b>' + stones[0] +
 	'</b>';
@@ -61,6 +64,50 @@ function displayStones() {
     document.getElementById("box3").innerHTML = '<b>' + stones[2] +
         '</b>';
     return 0;
+}
+
+/*
+ * COMPUTER PLAYER -- PERFECT
+ *
+ * This will make the perfect move, if such a move exists.
+ * If it can't make a perfect move, it will remove one stone from a
+ * random pile.  This is the computer player.
+ * returns [pile, amount removed]
+ */
+function perfectMove() {
+    var n = 0;
+    if( (stones[0] + stones[1] + stones[2]) == 0 ) {
+	return [-1,-1];
+    }
+    if( (stones[0] ^ stones[1] ^ stones[2]) == 0 ) {
+	// There is no perfect move
+	console.log("I can't move perfectly, so just take one.");
+	n = randBetween(0,2);
+	while( stones[n] == 0 ) {
+	    n = (n+1)%3;
+	}
+	stones[n]--;
+	return [n, 1];
+    }
+    for( n = 0; n < 3; ++n ) {
+	for( var removed=1; removed <= stones[n]; ++removed ) {
+	    var x;
+	    if( n == 0 ) {
+		x = stones[1] ^ stones[2] ^ (stones[n] - removed);
+	    }
+	    if( n == 1 ) {
+		x = stones[0] ^ stones[2] ^ (stones[n] - removed);
+	    }
+	    if( n == 2 ) {
+		x = stones[1] ^ stones[0] ^ (stones[n] - removed);
+	    }
+	    if( x == 0 ) {
+		stones[n] -= removed;
+		return [n, removed];
+	    }
+	}
+    }
+    return [-1, -1];
 }
 
 stones = pickthree( lowest, highest );
