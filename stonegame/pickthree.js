7dca9b086ebd6397591f64f364058e5225fe5f20
Kevin Brunelle <brunellek@gmail.com>
2016-08-16 20:09:19 -0400

    Complete the basic game

--- stonegame/pickthree.js
+++ stonegame/pickthree.js
@@ -1,6 +1,13 @@
 var lowest = 3;
 var highest = 18;
 var stones;
+var whoseturn = 0;
+var turn = 0;
+var p = [];
+p[0] = { human: 1,
+	 name: "Kevin" };
+p[1] = { human: 0,
+	 name: "Computer" };
 
 /*
  * A random number between two values inclusive.
@@ -67,6 +74,102 @@ function displayStones() {
 }
 
 /*
+ * Button press
+ */
+function bPress(numb) {
+    var x;
+    x = document.getElementById("in" + numb).value;
+    console.log("[ " + numb + ", " + x + " ]");
+    if( x <= stones[numb] ) {
+	stones[numb] -= x;
+	displayInfo(numb,x);
+	nextTurn();
+    }
+}
+
+function stoneCount() {
+    return (stones[0]+stones[1]+stones[2]);
+}
+
+function winner() {
+    displayStones();
+    displayButtons(p[whoseturn].human);
+    document.getElementById("gamesetup").innerHTML = "<h2>" + p[whoseturn].name
+	+ " won!</h2>";
+}
+
+/*
+ * Next Turn
+ */
+function nextTurn() {
+    if ( stoneCount() == 0 ) {
+	winner();
+    } else {
+	turn++;
+	whoseturn = (whoseturn + 1) % 2;
+	if(p[whoseturn].human == 0) {
+	    perfectMove();
+	}
+	displayButtons();
+	displayStones();
+	displayPlayer();
+    }
+}
+
+function displayInfo(n, move) {
+    id = "info" + (n+1);
+    console.log(p[whoseturn]);
+    document.getElementById(id).innerHTML += "<b>Turn " + turn + ": "
+	+ p[whoseturn].name + " took " + move + " stones.</b><br>";
+}
+
+function displayPlayer() {
+    document.getElementById("gamesetup").innerHTML = '<h2>' +
+	p[whoseturn].name + "'s Turn</h2>";
+}
+
+/*
+ *
+ */
+function displayButtons() {
+    if(p[whoseturn].human == 0) {
+	// This is a computer, just continue buttons
+	document.getElementById("but1").innerHTML = '<button type="button"'
+	   + 'onclick="nextTurn()"' + '>CONTINUE</button>';
+	document.getElementById("but2").innerHTML = '<button type="button"'
+	    + 'onclick="nextTurn()"' + '>CONTINUE</button>';
+	document.getElementById("but3").innerHTML = '<button type="button"'
+	    + 'onclick="nextTurn()"' + '>CONTINUE</button>';
+	return 0;
+    }
+    if( stones[0] > 0 ) {
+	document.getElementById("but1").innerHTML = '<form><input type="number"'
+	    + 'min="1" max="' + stones[0] + '" value="1" id="in0">'
+	    + '<button type="button"'
+	    + 'onclick="bPress(0)"' + '>CONTINUE</button>';
+    } else {
+	document.getElementById("but1").innerHTML = "";
+    }
+    if( stones[1] > 0 ) {
+	document.getElementById("but2").innerHTML = '<form><input type="number"'
+	    + 'min="1" max="' + stones[1] + '" value="1" id="in1">'
+	    + '<button type="button"'
+	    + 'onclick="bPress(1)"' + '>CONTINUE</button>';
+    } else {
+	document.getElementById("but2").innerHTML = "";
+    }
+    if( stones[2] > 0 ) {
+	document.getElementById("but3").innerHTML = '<form><input type="number"'
+	    + 'min="1" max="' + stones[2] + '" value="1" id="in2">'
+	    + '<button type="button"'
+	    + 'onclick="bPress(2)"' + '>CONTINUE</button>';
+    } else {
+	document.getElementById("but3").innerHTML = "";
+    }
+    return 0;
+}
+
+/*
  * COMPUTER PLAYER -- PERFECT
  *
  * This will make the perfect move, if such a move exists.
@@ -91,6 +194,7 @@ function perfectMove() {
 	    n = (n+1)%3;
 	}
 	stones[n]--;
+	displayInfo(n, 1);
 	return [n, 1];
     }
     for( n = 0; n < 3; ++n ) {
@@ -107,6 +211,7 @@ function perfectMove() {
 	    }
 	    if( x == 0 ) {
 		stones[n] -= removed;
+		displayInfo(n, removed);
 		return [n, removed];
 	    }
 	}
@@ -115,3 +220,7 @@ function perfectMove() {
 }
 
 stones = pickthree( lowest, highest );
+window.onload = function() {
+    whoseturn = randBetween(0,1);
+    nextTurn();
+}
