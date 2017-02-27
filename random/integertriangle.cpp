d42afd0d2190bf447c72e2df7c482bbe42c8d128
Kevin Brunelle <brunellek@gmail.com>
2017-02-27 11:10:15 -0500

    Change to smaller O() function

--- random/integertriangle.cpp
+++ random/integertriangle.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <iostream>
+#include <cmath>
 
 void output_solution(int, int, int);
 int find_solutions(int);
@@ -11,24 +12,18 @@ void output_solution(int a, int b, int c) {
   std::cout << "a: " << a << "\tb: " << b << "\tc: " << c << std::endl;
 }
 
-/*
- * I could avoid using the second loop if I did sqrt(cPow2-aPow2)
- * and checked if that was an integer solution. But, for small values
- * of c, I am not sure if that will be faster than this method. More
- * testing for optimization should be done.
- */
 int find_solutions(int c) {
   int cPow2 = c * c;
-  int a, b, aPow2, bPow2;
+  int a, b, aPow2, bPow2, max = c;
   int solutions = 0;
-  for( a = 1; a < c; ++a ) {
-    for( b = a; b < c; ++b ) {
-      aPow2 = a * a;
-      bPow2 = b * b;
-      if( cPow2 == (bPow2 + aPow2) ) {
-	output_solution(a, b, c);
-	solutions++;
-      }
+  for( a = 1; a < max; ++a ) {
+    aPow2 = a * a;
+    b = static_cast<int>(std::sqrt( static_cast<double>(cPow2 - aPow2) ));
+    bPow2 = b * b;
+    if( cPow2 == (bPow2 + aPow2) ) {
+      max = b;
+      output_solution(a, b, c);
+      solutions++;
     }
   }
   return solutions;
