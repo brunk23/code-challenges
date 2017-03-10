c0fb2e3a19e84636a677b0cd8ac52df30649d5f3
Kevin Brunelle <brunellek@gmail.com>
2017-03-10 08:25:13 -0500

    Clean up main loop
    Remove the conditionals to the test function itself, as they better
    belong inside it and the main loop is clearer and easier to understand
    with them out of it.  The only downside is passing so many arguments
    to the test function, and it no longer returns a value.

--- projecteuler/projecteuler033.cpp
+++ projecteuler/projecteuler033.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
-int gcd(int,int);
-bool test(int num, int den, int n, int d);
+int gcd(int a, int b);
+void test(int num, int den, int n, int d, int j);
 void winner(int num, int den, int digit);
 
 using namespace std;
@@ -10,32 +10,34 @@ using namespace std;
  * Get the GCD, which is required for many tests
  */
 int gcd(int a, int b) {
-    int temp;
-    while(b) {
-        temp = b;
-        b = a % b;
-        a = temp;
-    }
-    return a;
+  int temp;
+  while(b) {
+    temp = b;
+    b = a % b;
+    a = temp;
+  }
+  return a;
 }
 
 /*
  * If a fraction less than 1 and both are equivalent, then
  * this would be 1 of the 4 winners.
  */
-bool test(int num, int den, int n, int d)
+void test(int num, int den, int n, int d, int j)
 {
-    int f;
-    if( num >= den ) {
-        return false;
-    }
-    f = gcd(num,den);
-    num /= f;
-    den /= f;
-    f = gcd(n,d);
-    n /= f;
-    d /= f;
-    return ( (num==n) && (den==d) );
+  int f, testnum, testden;
+  if( num >= den ) {
+    return;
+  }
+  f = gcd(num,den);
+  testnum = num / f;
+  testden = den / f;
+  f = gcd(n,d);
+  n /= f;
+  d /= f;
+  if ( (testnum==n) && (testden==d) ) {
+    winner(num, den, j);
+  }
 }
 
 /*
@@ -45,41 +47,37 @@ bool test(int num, int den, int n, int d)
  */
 void winner(int num, int den, int digit)
 {
-    static int numer = 1;
-    static int denom = 1;
-    int f;
-    numer *= num;
-    denom *= den;
-    f = gcd(numer, denom);
-    numer /= f;
-    denom /= f;
-    cout << num << "/" << den << " :: " << digit << "\t";
-    cout << "CURRENT PRODUCT: " << numer << "/" << denom << endl;
+  static int numer = 1;
+  static int denom = 1;
+  int f;
+  numer *= num;
+  denom *= den;
+  f = gcd(numer, denom);
+  numer /= f;
+  denom /= f;
+  cout << num << "/" << den << " :: " << digit << "\t";
+  cout << "CURRENT PRODUCT: " << numer << "/" << denom << endl;
 }
 
 int main()
 {
-    int n,d,j;
+  int n,d,j;
 
-    /*
-     * Generate all the fraction combinations, and test for a winner.
-     * This does more work than it needs to, but the problem set is
-     * small enough to brute force.
-     */
-    for(n = 1; n <= 9; ++n) {
-        for(d = 1; d <= 9; ++d) {
-            for(j = 1; j <= 9; ++j) {
-                if( test((n*10)+j, (d*10)+j, n, d ) )
-                        winner((n*10)+j, (d*10)+j, j);
-                if( test((n*10)+j, (j*10)+d, n, d ) )
-                        winner((n*10)+j, (j*10)+d, j);
-                if( test((j*10)+n, (d*10)+j, n, d ) )
-                        winner((j*10)+n, (d*10)+j, j);
-                if( test((j*10)+n, (j*10)+d, n, d ) )
-                        winner((j*10)+n, (j*10)+d, j);
-            }
-        }
+  /*
+   * Generate all the fraction combinations, and test for a winner.
+   * This does more work than it needs to, but the problem set is
+   * small enough to brute force.
+   */
+  for(n = 1; n <= 9; ++n) {
+    for(d = 1; d <= 9; ++d) {
+      for(j = 1; j <= 9; ++j) {
+	test((n*10)+j, (d*10)+j, n, d, j);
+	test((n*10)+j, (j*10)+d, n, d, j);
+	test((j*10)+n, (d*10)+j, n, d, j);
+	test((j*10)+n, (j*10)+d, n, d, j);
+      }
     }
+  }
 
-    return 0;
+  return 0;
 }
