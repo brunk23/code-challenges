e7f9976e6517e401071f6bbb3ba21143c5fd9f87
Kevin Brunelle <brunellek@gmail.com>
2017-11-25 16:28:58 -0500

    Day 18 done

---
+++ AdventOfCode2016/Day18/day18-2.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+string floor[400000];
+
+bool isTrap(int, int);
+
+bool isTrap(int row, int col) {
+  bool state = false;
+  bool l, c, r;
+
+  if( (col == 0) || (floor[row-1][col-1] == '.' ) ) {
+    l = false;
+  } else {
+    l = true;
+  }
+
+  if( (col == floor[0].length() -1) || (floor[row-1][col+1] == '.') ) {
+    r = false;
+  } else {
+    r = true;
+  }
+
+  if( floor[row-1][col] == '.' ) {
+    c = false;
+  } else {
+    c = true;
+  }
+
+  if( (l && c) && !r ) {
+    return true;
+  }
+
+  if( (c && r) && !l ) {
+    return true;
+  }
+
+  if( !(c || r) && l ) {
+    return true;
+  }
+
+  if( !(l || c) && r ) {
+    return true;
+  }
+  return false;
+}
+
+int main() {
+  int i, c, count = 0;
+
+  for(i = 0; i < 400000; i++ ) {
+    floor[i] = "";
+  }
+  cin >> floor[0];
+
+  for( i = 1; i <400000; i++ ) {
+    for(c = 0; c < floor[0].length(); c++ ) {
+      if( isTrap(i,c) ) {
+	floor[i] += '^';
+      } else {
+	floor[i] += '.';
+      }
+    }
+  }
+
+  for(i = 0; i < 400000; i++ ) {
+    for( c = 0; c < floor[0].length(); c++) {
+      if( floor[i][c] == '.' ) {
+	count++;
+      }
+    }
+    cout << floor[i] << endl;
+  }
+  cout << count << endl;
+  return 0;
+}
