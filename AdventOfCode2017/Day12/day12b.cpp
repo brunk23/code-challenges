aa3d9c907fe8d147b460b9569ca19f93a5e09010
Kevin Brunelle <brunellek@gmail.com>
2017-12-12 13:13:07 -0500

    Finish day 12
    Another pretty easy day.

---
+++ AdventOfCode2017/Day12/day12b.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+
+using std::cin;
+using std::cout;
+using std::cerr;
+using std::endl;
+using std::stringstream;
+using std::string;
+using std::getline;
+
+#define NUMBER 2000
+#define MAXWIRE 10
+
+class Prog {
+public:
+  bool visited;
+  int wires[MAXWIRE];
+  
+  Prog() {
+    int i;
+    visited = false;
+    for( i = 0; i < MAXWIRE; i++ ) {
+      wires[i] = -1;
+    }
+  }
+  
+  ~Prog() {
+  }
+};
+
+bool isValid(int);
+void visit(Prog a[NUMBER], int dest);
+
+bool isValid(int i) {
+  if( i >= 0 && i < NUMBER ) {
+    return true;
+  }
+  return false;
+}
+
+void visit(Prog a[NUMBER], int dest) {
+  int i;
+  if( a[dest].visited ) {
+    return;
+  }
+  a[dest].visited = true;
+  for(i = 0; i < MAXWIRE; i++ ) {
+    if( a[dest].wires[i] > -1 ) {
+      visit(a, a[dest].wires[i]);
+    }
+  }
+}
+
+int main() {
+  Prog a[NUMBER];
+  string input;
+  int i, value, j;
+
+  while( getline(cin, input) ) {
+    for( i = 0; i < input.length(); i++ ) {
+      if( !( (input[i]>='0') && (input[i]<='9') ) ) {
+	input[i] = ' ';
+      }
+    }
+    stringstream s(input);
+    if( !( s >> i ) ) {
+      cerr << "What the heck! " << input << endl;
+      return 1;
+    }
+    if( !isValid(i) ) {
+      cerr << "Not a valid index. " << input << endl;
+      return 1;
+    }
+    j = 0;
+    while( s >> value ) {
+      if( isValid(value) ) {
+	a[i].wires[j] = value;
+	j++;
+      } else {
+	cerr << "Not a valid index. " << input << endl;
+      }
+    }
+  }
+
+  j = 0;
+  for( i = 0; i < NUMBER; i++ ) {
+    if( !(a[i].visited) ) {
+      j++;
+      visit(a,i);
+      cout << "Group starts at: " << i << endl;
+    }
+  }
+
+  cout << "Total groups " << j << endl;
+
+  return 0;
+}
