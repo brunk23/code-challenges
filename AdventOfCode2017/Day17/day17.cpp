e2a484d17f23b4760f378dcedcf7bae22dffb478
Kevin Brunelle <brunellek@gmail.com>
2017-12-17 00:44:12 -0500

    Second part isn't too bad
    Once you realize that you don't need to track anything. Just when
    you add a new number after 0. I wonder if there was an easy way
    to do that for the first part.  I feel like there should be, but
    I just generated the complete buffer.

---
+++ AdventOfCode2017/Day17/day17.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class CBuff {
+private:
+  int len;
+  int stepsize;
+  int pos;
+  int curr;
+  int *buffer;
+
+  void shiftup(int start) {
+    int i;
+    for( i = len-2; i >= start; i-- ) {
+      buffer[i+1] = buffer[i];
+    }
+  }
+  
+public:
+  CBuff(int size) {
+    int i;
+    len = 2018;
+    buffer = new int[len];
+    stepsize = size;
+    
+    curr = 0;
+    pos = 0;
+    buffer[0] = 0;
+    for( i = 1; i < len; i++ ) {
+      buffer[i] = -1;
+    }
+  }
+
+  int step() {
+    int i;
+    curr++;
+
+    i = (stepsize + pos) % curr;
+
+    if( buffer[i+1] != -1 ) {
+      shiftup(i+1);
+    }
+    buffer[i+1] = curr;
+    pos = i+1; 
+
+    return buffer[pos+1];
+  }
+};
+
+int main() {
+  int i;
+  CBuff b(328);
+  
+  for(i = 0; i < 2017; i++ ) {
+    cout << i+1 << ": " << b.step() << endl;
+  }
+  return 0;
+}
+  
---
+++ AdventOfCode2017/Day17/day17b.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+
+using std::cout;
+using std::endl;
+
+class CBuff {
+private:
+  int stepsize;
+  int pos;
+  int curr;
+  int old;
+  
+public:
+  CBuff(int size) {
+    stepsize = size;
+    
+    curr = 0;
+    pos = 0;
+    old = -1;
+  }
+
+  int step() {
+    int i;
+    curr++;
+
+    i = (stepsize + pos) % curr;
+
+    if( i == 0 ) {
+      old = curr;
+    }
+    pos = i + 1;
+    
+    return old;
+  }
+};
+
+int main() {
+  int i, c;
+  CBuff b(328);
+  
+  for(i = 1; i < 50000000; i++ ) {
+    c = b.step();
+  }
+  cout << c << endl;
+  return 0;
+}
+  
