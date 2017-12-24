8acd9599c2b7b3b69d62f9a079fc503751691103
Kevin Brunelle <brunellek@gmail.com>
2017-12-24 18:22:52 -0500

    Fix day 5
    Now works with both inputs. Also, had an uninitialized variable

--- AdventOfCode2017/Day05/day05.cpp
+++ AdventOfCode2017/Day05/day05.cpp
@@ -1,28 +1,29 @@
 #include <iostream>
 
-#define COUNT 1003
+#define COUNT 1099
 
 using std::cin;
 using std::cout;
 using std::endl;
 
 int main() {
-  int maze[COUNT], i, input, curr, prev;
+  int maze[COUNT], i = 0, input, curr, prev;
+  int max;
 
   while( cin >> input ) {
     maze[i] = input;
     i++;
   }
-
+  max = i;
   i = 0;
   curr = 0;
-  while( (curr >= 0) && (curr < COUNT) ) {
+  while( (curr >= 0) && (curr < max) ) {
     prev = curr;
     curr += maze[curr];
     maze[prev]++;
     i++;
-    cout << i << ": " << curr << endl;
+    // cout << i << ": " << curr << endl;
   }
-  
+  cout << i << endl;
   return 0;
 }
