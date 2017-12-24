951e2f9f0ff750d062eb03fc1818d820afb4df83
Kevin Brunelle <brunellek@gmail.com>
2017-12-24 16:54:23 -0500

    Generalize my fast solution
    It dynamically resizes the map, as needed, to contain the problem
    set. Works for my original input and the other accounts'.

--- AdventOfCode2017/Day22/nodep2o.h
+++ AdventOfCode2017/Day22/nodep2o.h
@@ -4,38 +4,47 @@
 #include <iostream>
 #include <string>
 
-enum States { clean, weakened, infected, flagged };
+// Adding 1 turns right, sub 1 turns left
+enum Directions { up, right, down, left };
+
+const char clean = '.';
+const char weakened = 'w';
+const char infected = '#';
+const char flagged = '~';
 
 class Node {
  private:
-  struct iNode {
-    int x;
-    int y;
-    int state;
-  };
 
-  iNode map[428][323];
+  typedef char iNode;
+
+ iNode *map;
   unsigned int maxs[2];
+  int xmax;
+  int ymax;
   int xoff;
   int yoff;
 
  public:
   Node() {
     int i, j;
-    for( i = 0; i < 428; i++ ) {
-      for( j = 0; j < 323; j++ ) {
-	map[i][j].x = i;
-	map[i][j].y = j;
-	map[i][j].state = clean;
+    xmax = 26;
+    ymax = 26;
+    map = new iNode[ xmax * ymax ];
+    for( i = 0; i < xmax; i++ ) {
+      for( j = 0; j < ymax; j++ ) {
+	map[i * ymax + j] = clean;
       }
     }
     maxs[0] = 0;
     maxs[1] = 0;
-    xoff = 184;
-    yoff = 146;
+    xoff = 0;
+    yoff = 0;
   };
 
   ~Node() {
+    delete map;
+    std::cerr << "End Dimensions: " << xmax << "x"
+	      << ymax << std::endl;
   };
 
   unsigned int maxX() {
@@ -55,42 +64,115 @@ class Node {
     if( row.length() > maxs[1] ) {
       maxs[1] = row.length();
     }
-    
+
+    if( maxs[0] >= xmax ) {
+      resize(down);
+    }
+
+    if( maxs[1] >= ymax ) {
+      resize(right);
+    }
+
     for( i = 0; i < row.length(); i++ ) {
       if( row[i] == '#' ) {
-	map[rownumber+xoff][i+yoff].state = infected;
+	map[(rownumber + xoff) * ymax + i + yoff] = infected;
       }
     }
   };
 
-  /*
-   * Changes last. It's the first step
-   * so clean() and infect() can avoid
-   * the lookup costs.
-   */
-  int status(int loc[2]) {
-    return map[loc[0]+xoff][loc[1]+yoff].state;
+  void resize(int dir) {
+    int nxmax = xmax, nymax = ymax, i, j, k = 0, m = 0;
+    iNode *foo;
+    int inc = 10;
+
+    if( dir == down || dir == up ) {
+      nxmax += inc;
+    } else {
+      nymax += inc;
+    }
+    if( dir == up ) {
+      xoff += inc;
+    }
+    if( dir == left ) {
+      yoff += inc;
+    }
+    iNode *tmp = new iNode[nxmax * nymax];
+    for( i = 0; i < nxmax; i++ ) {
+      for( j = 0; j < nymax; j++ ) {
+	tmp[i * nymax + j] = clean;
+      }
+    }
+    if( dir == up ) {
+      k = inc;
+    }
+    if( dir == left ) {
+      m = inc;
+    }
+    for( i = 0; i < xmax; i++ ) {
+      for( j = 0; j < ymax; j++ ) {
+	tmp[(i+k)*nymax + (j+m)] = map[i*ymax + j];
+      }
+    }
+    foo = map;
+    map = tmp;
+    delete foo;
+    xmax = nxmax;
+    ymax = nymax;
+  };
+
+  void printmap(char m[], int x, int y) {
+    int i, j;
+    for( i = 0; i < x; i++ ) {
+      for( j = 0; j < y; j++ ) {
+	std::cout << m[i*y + j];
+      }
+      std::cout << std::endl;
+    }
+    std::cout << std::endl;
+  };
+
+  void checkloc(int loc[2]) {
+    if( loc[0] + xoff >= xmax ) {
+      resize(down);
+    }
+    if( loc[0] + xoff < 0 ) {
+      resize(up);
+    }
+    if( loc[1] + yoff >= ymax ) {
+      resize(right);
+    }
+    if( loc[1] + yoff < 0 ) {
+      resize(left);
+    }
+  };
+
+  char status(int loc[2]) {
+    checkloc(loc);
+    return map[(loc[0] + xoff) * ymax + loc[1] + yoff];
   };
   
   void chgstate(int loc[2]) {
-    switch( map[loc[0]+xoff][loc[1]+yoff].state ) {
+    int calc;
+    checkloc(loc);
+    calc = (loc[0] + xoff) * ymax + loc[1] + yoff;
+    switch( map[calc] ) {
     case clean:
-      map[loc[0]+xoff][loc[1]+yoff].state = weakened;
+      map[calc] = weakened;
       break;
     case weakened:
-      map[loc[0]+xoff][loc[1]+yoff].state = infected;
+      map[calc] = infected;
       break;
     case infected:
-      map[loc[0]+xoff][loc[1]+yoff].state = flagged;
+      map[calc] = flagged;
       break;
     case flagged:
-      map[loc[0]+xoff][loc[1]+yoff].state = clean;
+      map[calc] = clean;
       break;
     default:
       break;
     }
   };
-  
+
 };
 
 #endif
