e31ae279a2e5e67584dd4f65d8d279c8c3e600d7
Kevin Brunelle <brunellek@gmail.com>
2017-12-27 11:21:08 -0500

    Allow dynamic setting of b
    This also prints both possible answers every time. This program
    does work for setting f to a different value. Does not work if
    we change the loop for setting b and c when a == 1, then again,
    the previous version didn't work for that either.

--- AdventOfCode2017/extra/test.cpp
+++ AdventOfCode2017/extra/test.cpp
@@ -1,18 +1,27 @@
 #include <iostream>
 
 int main() {
-  long a = 1, b = 86, c = b, f = 17, h = 0;
+  long b = 86, c = b, f = 17, h = 0;
 
-  if( a != 0 ) {
-    b *= 10946;
-    c *= 17711;
+  std::cout << "What should we use for b (86 default): ";
+  if( !(std::cin >> b) || (b <= 0)  ) {
+    b = 86;
   }
 
+  std::cout << "set b " << b << std::endl;
+  c = b;
   h = (1 << f) - 1;
 
   f = (b * (2 * c - b + 1)) / 2 % h;
 
-  std::cout << "If A = " << a << ": F = " << f << std::endl;
+  std::cout << "If A = 0: F = " << f << std::endl;
+
+  b *= 10946;
+  c *= 17711;
+
+  f = (b * (2 * c - b + 1)) / 2 % h;
+
+  std::cout << "If A = 1: F = " << f << std::endl;
 
   return 0;
 }
