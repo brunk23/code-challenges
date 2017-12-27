2d434379f0db311226264394b035e2e5a1065581
Kevin Brunelle <brunellek@gmail.com>
2017-12-27 08:59:34 -0500

    Get rid of comments in c++ code
    It is fully commented in the disassembly file

--- AdventOfCode2017/extra/test.cpp
+++ AdventOfCode2017/extra/test.cpp
@@ -1,41 +1,17 @@
 #include <iostream>
 
-long triangle(long a) {
-  return (a * (a+1))/2;
-}
-
 int main() {
-  long a=1, b= 86, c=86, d=0,e=0,f=0,g=0,h=0;
-  
-  for( a = a * 10; a != 0; a-- ) {
-    b += c;
-    c += b;
+  long a = 0, b = 86, c = b, f = 17, h = 0;
+
+  if( a != 0 ) {
+    b *= 10946;
+    c *= 17711;
   }
 
-  h = 1;
-  for( f = 17; f > 0; f--) {
-    h += h;
-  }
-  h--;
-
-  std::cout << "A = 0: " << (86 * (2 * 86 - 86 + 1)) / 2 % h << std::endl;
-  
-  // This should be the same as below
-  // std::cout << (b*(c+1) - triangle(b))%h << std::endl;
+  h = (1 << f) - 1;
 
   // Combined into 1 line
-  std::cout << "A = 1: " << (b * (2 * c - b + 1)) / 2 % h << std::endl;
-
-  // This appears to be a variation of the triangular numbers
-  d = 1;
-  for( d = 1; d <= b; d++ ) {
-    f += c - (d - 1);
-    f %= h;
-  }
-
-  std::cout << "a: " << a << "\tb: " << b << "\tc: " << c
-	    << "\td: " << d << "\te:  " << e << "\tf: "
-	    << f << "\tg: " << g << "\th: " << h << std::endl;
+  std::cout << "A = " << a << ": " << (b * (2 * c - b + 1)) / 2 % h << std::endl;
   
   return 0;
 }
