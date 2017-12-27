ebe01edba5d755d4d544848718c229c3d39c4924
Kevin Brunelle <brunellek@gmail.com>
2017-12-27 10:57:55 -0500

    difficulty.sh update

--- AdventOfCode2017/difficulty.sh
+++ AdventOfCode2017/difficulty.sh
@@ -4,6 +4,7 @@
 # second part of each day, by ranking the percentage of people
 # who got the first part but not the second.
 
-awk '{printf "%0.2f", 100 * $3 / ( $2 + $3 );\
-	print "%\tout of\t" $2 + $3\
-       	"\tonly did part one of Day " $1}' | sort -rn
+awk 'BEGIN{total = 0}{ n = 100 * $3 / ( $2 + $3 ); total = total + n;\
+	printf "%0.2f", n; print "%\tout of\t" $2 + $3\
+	"\tonly did part one of Day " $1}\
+	END{printf "\n%s%0.2f%%\n", "Average day: ", total/25}' | sort -rn
