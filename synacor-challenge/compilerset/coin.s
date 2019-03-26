b1d0f5d254278ae64676c21d857909739278760a
Kevin Brunelle <brunellek@gmail.com>
2019-03-26 11:19:40 -0400

    Clean up mistake

--- synacor-challenge/compilerset/coin.s
+++ synacor-challenge/compilerset/coin.s
@@ -1,122 +1,122 @@
-.origin 00000
-	setr1disp
-	callpstr
-	out10
-inp:	callreadnum
-	jfr1tryagain
-	gtr0r1499
-	jtr0tryagain
-	setr0r1
+	.origin	00000
+	set	r1	disp
+	call	pstr
+	out	10
+inp:	call	readnum
+	jf	r1	tryagain
+	gt	r0	r1	499
+	jt	r0	tryagain
+	set	r0	r1
 
 	# This is our adding loop
 addvalues:
-	setr1vals# point r1 to vals
-	rmemr2r1# get the first number
+	set	r1	vals		# point r1 to vals
+	rmem	r2	r1		# get the first number
 addvalues1:
-	addr1r11# increase r1
-	rmemr3r1# get the val
-	addr2r2r3# add next value
-	eqr7lastvr1# if r1 != 6
-	jfr7addvalues1# loop again
+	add	r1	r1	1	# increase r1
+	rmem	r3	r1		# get the val
+	add	r2	r2	r3	# add next value
+	eq	r7	lastv	r1	# if r1 != 6
+	jf	r7	addvalues1	# loop again
 
-	eqr7r0r2# if r0 == r2
-	jtr7hitvalue
-	gtr7r2r0# if r2 > r0
-	jtr7overvalue
+	eq	r7	r0	r2	# if r0 == r2
+	jt	r7	hitvalue
+	gt	r7	r2	r0	# if r2 > r0
+	jt	r7	overvalue
 
-	rmemr3vals
-	rmemr2coins
-	addr3r3r2
-	wmemvalsr3
-	jmpaddvalues
+	rmem	r3	vals
+	rmem	r2	coins
+	add	r3	r3	r2
+	wmem	vals	r3
+	jmp	addvalues
 
 hitvalue:
-	addr6r61
-	eqr7r610000
-	jtr7upcount
+	add	r6	r6	1
+	eq	r7	r6	10000
+	jt	r7	upcount
 hitvalue2:
-	setr4strgs
-	setr5coins
-	setr3vals
-jfcount:	hitvalue0
-	rmemr1count
-	callpnumber
-	gtr7r69
-	jfr7three0s
-	gtr7r699
-	jfr7two0s
-	gtr7r6999
-	jfr7one0
-	jmphitvalue0
+	set	r4	strgs
+	set	r5	coins
+	set	r3	vals
+	jf	count:	hitvalue0
+	rmem	r1	count
+	call	pnumber
+	gt	r7	r6	9
+	jf	r7	three0s
+	gt	r7	r6	99
+	jf	r7	two0s
+	gt	r7	r6	999
+	jf	r7	one0
+	jmp	hitvalue0
 three0s:
-	out48
-two0s:	out48
-one0:	out48
+	out	48
+two0s:	out	48
+one0:	out	48
 hitvalue0:
-	setr1r6
-	callpnumber
-	out9
+	set	r1	r6
+	call	pnumber
+	out	9
 hitvalue1:
-	 rmemr1r4
-	callpstr
-	rmemr1r3
-	rmemr2r5
-	calldivide
-	callpnumber
-	out9
-	addr3r31
-	addr4r41
-	addr5r51
-	eqr7r3coins
-	jfr7hitvalue1
-	out10
+	rmem	r1	r4
+	call	pstr
+	rmem	r1	r3
+	rmem	r2	r5
+	call	divide
+	call	pnumber
+	out	9
+	add	r3	r3	1
+	add	r4	r4	1
+	add	r5	r5	1
+	eq	r7	r3	coins
+	jf	r7	hitvalue1
+	out	10
 overvalue:
-	setr5vals
-	setr3coins
+	set	r5	vals
+	set	r3	coins
 overvalue1:
-	rmemr2r5
-	addr5r51
-	addr3r31
-	eqr7r5coins# we are out of coins
-	jtr7endprogram
-	eqr70r2# if r2 == 0
-	jtr7overvalue1# go to next value
-	rmemr2r5# r2 = m[1]
-	rmemr3r3# r3 = m[r3]
-	addr2r3r2# r2 = r3 + r2
-	wmemr5r2# m[r1] = r2
-	addr5r532767# r1 = r1 - 1
-	wmemr50
-	jmpaddvalues
+	rmem	r2	r5
+	add	r5	r5	1
+	add	r3	r3	1
+	eq	r7	r5	coins	# we are out of coins
+	jt	r7	endprogram
+	eq	r7	0	r2	# if r2 == 0
+	jt	r7	overvalue1	# go to next value
+	rmem	r2	r5		# r2 = m[1]
+	rmem	r3	r3		# r3 = m[r3]
+	add	r2	r3	r2	# r2 = r3 + r2
+	wmem	r5	r2		# m[r1] = r2
+	add	r5	r5	32767	# r1 = r1 - 1
+	wmem	r5	0
+	jmp	addvalues
 endprogram:
 	halt
 
 tryagain:
-	setr1emsg
-	callpstr
-	jmpinp
+	set	r1	emsg
+	call	pstr
+	jmp	inp
 
 upcount:
-	rmemr1count
-	addr1r11
-	wmemcountr1
-	setr60
-	jmphitvalue2
+	rmem	r1	count
+	add	r1	r1	1
+	wmem	count	r1
+	set	r6	0
+	jmp	hitvalue2
 
-	.include"readnum.s"
-	.include"divide.s"
-	.include"pstr.s"
-	.include"pnumber.s"
+	.include	"readnum.s"
+	.include	"divide.s"
+	.include	"pstr.s"
+	.include	"pnumber.s"
 
-vals:	data0 0  0  0  0
+vals:	data	0 0  0  0  0
 lastv:	data    0
-coins:	data1 5 10 25 50 100
-disp:	data"How much cents? "
-emsg:	data"Pick a different number: "
-strgs:	datapenny nickel dime quarter half-dollar dollar
-penny:	data"Pennies: "
-nickel:	data"Nickels: "
-dime:	data"Dimes: "
-quarter:	data"Quarters: "
-half-dollar:	data"Halfs: "
-dollar:	data"Dollars: "
+coins:	data	1 5 10 25 50 100
+disp:	data	"How much cents? "
+emsg:	data	"Pick a different number: "
+strgs:	data	penny nickel dime quarter half-dollar dollar
+penny:	data	"Pennies: "
+nickel:	data	"Nickels: "
+dime:	data	"Dimes: "
+quarter:	data	"Quarters: "
+half-dollar:	data	"Halfs: "
+dollar:	data	"Dollars: "
