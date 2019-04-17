6bb3ca60a9cc39501982e365781de40c2df6ca83
Kevin Brunelle <brunellek@gmail.com>
2019-04-16 22:29:19 -0400

    Write another helper function

--- synacor-challenge/asmfiles/krypto.solver2.s
+++ synacor-challenge/asmfiles/krypto.solver2.s
@@ -17,43 +17,88 @@ get_nums:
 	add	r6	r6	1	; point to next location
 	add	r7	r7	32767	; decrement counter
 	jt	r7	get_nums	; loop while r7 > 0
+	set	r0	4		; We enter find_solution with n=4
 	call	find_solution
 	halt
 
 
 	;; find_solution() -- Does not need to preserve any registers
-	;; r6 = &c[i] r5 = c[i], r4 = &a[i] r3 = a[i], r2 = tmp
-	;; This is Heap's algorithm.
+	;; Needs to do "ab ac ad ae bc bd be cd ce de" and all the variations
+	;; Under that.
 find_solution:
-	set	r0	game_nums 	; r0 = a
-	set	r1	c		; r1 = c
-	set	r7	0		; r7 = i <- 0
-	set	r0	buffer
-	set	r1	25424
-	call	num2str
-	set	r2	r0
-	set	r1	str
-	call	append_str
-	call	pstr
+	eq	r7	r0	1
+	jt	r7	find_solution_done
+	set	r1	0
+	set	r2	1
+	call	copy_array
+	add	r0	r0	32767
+	call	find_solution
+find_solution_done:
 	ret
 
-	;; This will decrement the print buffer.
-zero_pbuffer:
+	;; copy_array()
+	;;
+	;; This will copy the array contents, except for the numbers
+	;; currently being picked as the two active numbers. Those will
+	;; be stored as the array[n-1][0] values
+	;;
+	;; Call method:
+	;;   set r0 n  -- index of array
+	;;   set r1 a  -- index of first number
+	;;   set r2 b  -- index of second number
+	;;   call copy_array
+	;;
+	;; Preserves all registers.  Doesn't save r1 and r2, but
+	;; also doesn't change them
+copy_array:
 	push	r0
-	push	r1
-	set	r0	70		; we have a 70 character
-	set	r1	buffer		; print buffer
-zero_pbuffer1:
-	wmem	r1	0		; write nulls
-	add	r1	r1	1	; increment address
-	add	r0	r0	32767	; decrement counter
-	jt	r0	zero_pbuffer1	; loop if not 0
-	pop	r1
+	push	r3
+	push	r4
+	push	r5
+	push	r6
+	push	r7
+	jf	r0	copy_array_done		; we are done if this is zero
+	add	r3	valsaddr	r0 	; Get addr of source values
+	add	r5	r3	32767		; Get addr of dest values
+	rmem	r3	r3			; r3 <- source values
+	add	r3	r3	r0		; point to last value
+	rmem	r5	r5			; r5 <- dest values
+	add	r5	r5	r0		; point to last value
+	add	r5	r5	32767		; which is one less than n
+	add	r4	strsaddr	r0	; Get addr of source string
+	add	r6	r4	32767		; Get addr of dest strings
+	rmem	r4	r4			; r4 <- source strings
+	add	r4	r4	r0		; point to last address
+	rmem	r6	r6			; r6 <- dest strings
+	add	r6	r6	r0		; pint to last string
+	add	r6	r6	32767		; one less than n
+copy_array_loop:
+	eq	r7	r0	r1    		; skip copy
+	jt	r7	copy_array_skip
+	eq	r7	r0	r2    		; skip copy
+	jt	r7	copy_array_skip
+	rmem	r7	r3	      		; read source value
+	wmem	r5	r7			; write to destination
+	rmem	r7	r4			; read source string
+	wmem	r6	r7			; write to destination
+copy_array_skip:
+	jf	r0	copy_array_done		; we copied the last value
+	add	r0	r0	32767		; r0--
+	add	r3	r3	32767		; r3--
+	add	r4	r4	32767		; r4--
+	add	r5	r5	32767		; r5--
+	add	r6	r6	32767		; r6--
+	jmp	copy_array_loop
+copy_array_done:
+	pop	r7
+	pop	r6
+	pop	r5
+	pop	r4
+	pop	r3
 	pop	r0
 	ret
 
 	.include	"utils/readnum.s"
-	.include	"utils/pnumber.s"
 	.include	"utils/bindivide.s"
 	.include	"utils/pstr.s"
 	.include	"utils/prepend_str.s"
@@ -68,14 +113,25 @@ value_ops:
 game_nums:
 	data	0	0	0	0	0
 goal:	data	0
-c:	data	0	0	0	0	0
 
-	;; Since we feed backwards for the string, we need
-	;; this buffer area.
-buffer:	data	0	0	0	0	0	0	0
-	data	0	0	0	0	0	0	0
-	data	0	0	0	0	0	0	0
-	data	0	0	0	0	0	0	0
-	data	0	0	0	0	0	0	0
-	data	0	0	0	0	0	0	0
-str:	data	" This is a test\0"
+	;; We use these strings for printing the results.
+openp:	data	"(\0"
+closep:	data	")\0"
+plus:	data	"+\0"
+minus:	data	"-\0"
+times:	data	"*\0"
+divid:	data	"/\0"
+
+	;; These will be used to pass the array addresses.
+valsaddr:
+	data	9009	9007	9004	9000	game_nums
+strsaddr:
+	data	9019	9017	9014	9010	wordstrs
+
+	;; These will be the string locations. This begs two possible
+	;; improvements to the assembler.  The ability to use equ statements
+	;; like "first equ 10000" and compiled math like "first+3" to create
+	;; 10003 automatically.
+wordstrs:
+	data	10000	10003	10006	10009	10012
+strs:	data	10115	10123	10136	10154 ; first, second, third, final
