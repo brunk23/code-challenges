032473cd017d90e6d4699c01b5db88f72c6a47f4
Kevin Brunelle <brunellek@gmail.com>
2020-12-02 18:52:46 -0500

    Day 2 -- Clean and Comment
    Clean up the code flow, avoid global variable use, and add more
    comments to explain how to use this file.

--- AdventOfCode2020/day02.el
+++ AdventOfCode2020/day02.el
@@ -1,5 +1,5 @@
-;; Needed for BOTH
-(defun daydata (file)
+;; Needed for BOTH parts
+(defun loaddaydata (file)
   "Read file into list of strings, one string per line."
   (with-current-buffer (find-file-noselect file)
     (split-string
@@ -10,9 +10,20 @@
 	(point-max)))
      "\n" t)))
 
-;; Needed for part A
+;; Needed for BOTH parts
+;; Make sure you load the correct isvalidp for the part you are answering
+(defun process (pfile)
+  "This will process the data list and count the valid passwords."
+  (let ((nvalid 0))
+    (dolist (passwd pfile)
+      (if (isvalidp passwd)
+	  (setq nvalid (+ 1 nvalid))))
+    (message "Valid passwords: %d" nvalid)))
+
+
+;; Needed for part one
 (defun isvalidp (passwd)
-  "Will return t, if the passwd is valid according to part a rules."
+  "Will return t, if the passwd is valid according to part one rules."
   (let* ((fields (split-string passwd " "))
 	 (range (mapcar 'string-to-number (split-string (car fields) "-")))
 	 (char (elt (elt fields 1) 0))
@@ -28,9 +39,9 @@
 	t
       nil)))
 
-;; Needed for part B
+;; Needed for part two
 (defun isvalidp (passwd)
-  "Will return t, if the passwd is valid according to part b rules."
+  "Will return t, if the passwd is valid according to part two rules."
   (let* ((fields (split-string passwd " "))
 	 (range (mapcar 'string-to-number (split-string (car fields) "-")))
 	 (char (elt (elt fields 1) 0))
@@ -43,20 +54,14 @@
 	  t
 	nil))))
 
-;; Needed for BOTH
-(defun process (pfile)
-  "This will process the data list and count the valid passwords."
-  (let ((nvalid 0))
-    (dolist (passwd pfile)
-      (if (isvalidp passwd)
-	  (setq nvalid (+ 1 nvalid))))
-    (message "Valid passwords: %d" nvalid)))
 
-;; Use the real data FOR ACTUAL
-(setq inp (daydata "./day02.input"))
+;; Once you have loaded the three functions above according to the part
+;; of the day you are on, select one of the following to get an answer.
+;; You can run the next part of the day by just loading the next
+;; isvalid and running this again.
 
-;; Use the sample data FOR TESTING
-(setq inp '("1-3 a: abcde" "1-3 b: cdefg" "2-9 c: ccccccccc"))
+;; Use the real data FOR ACTUAL ANSWER
+(process (loaddaydata "./day02.input"))
 
-;; This will perform the actual work
-(process inp)
+;; Use the sample data FOR TESTING PURPOSES
+(process '("1-3 a: abcde" "1-3 b: cdefg" "2-9 c: ccccccccc"))
