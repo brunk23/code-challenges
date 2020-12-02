65c315a2c9d5825704129e23dddf257d3b318657
Kevin Brunelle <brunellek@gmail.com>
2020-12-02 18:32:03 -0500

    Both parts are done.
    This is a little tricky, because you need to execute only those
    functions that make sense for your current situation. You load
    each function as required.  Then only load the isvalidp for the
    part of the day you are doing (a or b).  Then you can call process.
    
    You also can load the testing data or the real data.

--- AdventOfCode2020/day02.el
+++ AdventOfCode2020/day02.el
@@ -1,5 +1,6 @@
-;; Read the data file into a list, one string per line
+;; Needed for BOTH
 (defun daydata (file)
+  "Read file into list of strings, one string per line."
   (with-current-buffer (find-file-noselect file)
     (split-string
      (save-restriction
@@ -9,8 +10,56 @@
 	(point-max)))
      "\n" t)))
 
-;; Use the real data
+;; Needed for part A
+(defun isvalidp (passwd)
+  "Will return t, if the passwd is valid according to part a rules."
+  (let* ((fields (split-string passwd " "))
+	 (range (mapcar 'string-to-number (split-string (car fields) "-")))
+	 (char (elt (elt fields 1) 0))
+	 (pstr (elt fields 2))
+	 (len (length pstr))
+	 (count 0)
+	 (current 0))
+    (while (< current len)
+      (if (= char (elt pstr current))
+	  (setq count (+ 1 count)))
+      (setq current (+ 1 current)))
+    (if (and (>= count (elt range 0)) (<= count (elt range 1)))
+	t
+      nil)))
+
+;; Needed for part B
+(defun isvalidp (passwd)
+  "Will return t, if the passwd is valid according to part b rules."
+  (let* ((fields (split-string passwd " "))
+	 (range (mapcar 'string-to-number (split-string (car fields) "-")))
+	 (char (elt (elt fields 1) 0))
+	 (pstr (elt fields 2))
+	 (len (length pstr))
+	 (count 0)
+	 (current 0))
+    (if (and (= char (elt pstr (- (elt range 0) 1)))
+	     (not (= char (elt pstr (- (elt range 1) 1)))))
+	t
+      (if (and (= char (elt pstr (- (elt range 1) 1)))
+	       (not (= char (elt pstr (- (elt range 0) 1)))))
+	  t
+	nil))))
+
+;; Needed for BOTH
+(defun process (pfile)
+  "This will process the data list and count the valid passwords."
+  (let ((nvalid 0))
+    (dolist (passwd pfile)
+      (if (isvalidp passwd)
+	  (setq nvalid (+ 1 nvalid))))
+    (message "Valid passwords: %d" nvalid)))
+
+;; Use the real data FOR ACTUAL
 (setq inp (daydata "./day02.input"))
 
-;; Use the sample data
+;; Use the sample data FOR TESTING
 (setq inp '("1-3 a: abcde" "1-3 b: cdefg" "2-9 c: ccccccccc"))
+
+;; This will perform the actual work
+(process inp)
