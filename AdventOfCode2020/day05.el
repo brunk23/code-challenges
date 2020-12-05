573ff4f3bb31fc51ed4a63e032427640a1520e09
Kevin Brunelle <brunellek@gmail.com>
2020-12-05 12:17:03 -0500

    Day 5 Complete
    I simplified the seat list down to only IDs, as that was all that was
    needed and it cleaned up part 1 as well.  Then I only needed to sort
    and find a missing value.

--- AdventOfCode2020/day05.el
+++ AdventOfCode2020/day05.el
@@ -14,20 +14,30 @@
   "This just returns the sample data for test runs."
   '("BFFFBBFRRR" "FFFBBBFRRR" "BBFFBBFRLL"))
 
+;; Each seat description is actually a binary number where the letters B and R represent
+;; 1s and the letters F and L represent zeros.  The highest value bit is the first letter
+;; in the description. So, we just iterate through the string, and convert it into the
+;; right value by doubling and adding 1 when required.
+;; Originally this created a list of rows and cols for the seats, but that was unneccessary
+;; for either part of the solution, so I simplified it.
 (defun descrtoseat (str)
-  (let ((row 0)
-	(col 0)
+  "This will turn each description string into a seat ID number."
+  (let ((val 0)
 	(curr 0)
 	(max (length str)))
     (while (< curr max)
-      (cond ((= (elt str curr) ?B) (setq row (+ (* 2 row) 1)))
-	    ((= (elt str curr) ?F) (setq row (* 2 row)))
-	    ((= (elt str curr) ?R) (setq col (+ (* 2 col) 1)))
-	    ((= (elt str curr) ?L) (setq col (* 2 col))))
+      (cond ((= (elt str curr) ?B) (setq val (+ (* 2 val) 1)))
+	    ((= (elt str curr) ?F) (setq val (* 2 val)))
+	    ((= (elt str curr) ?R) (setq val (+ (* 2 val) 1)))
+	    ((= (elt str curr) ?L) (setq val (* 2 val))))
       (setq curr (1+ curr)))
-    (list row col)))
+    (if (= val 0)
+	(message "%s made a 0 value" str)
+      val)))
 
+;; Originally this created a list of lists, but we only need the seat ID numbers.
 (defun process (boardingpasses)
+  "This will convert the list of seat descriptions into seat ID numbers."
   (let ((seatlist nil)
 	(curr 0)
 	(max (length boardingpasses)))
@@ -36,16 +46,28 @@
       (setq curr (1+ curr)))
     seatlist))
 
+;; The simplest way to find the empty seat is to sort the list and find the part
+;; where it is non-contiguous. We are assured the missing number is not the first
+;; or the last value, and we are assured that it is the only missing value.
+(defun findmissing (seatlist)
+  "This will find the missing number in the sequence, assumes missing is not first/last."
+  (let* ((curr 1)
+	 (sortedseats (sort seatlist '<))
+	 (last (elt sortedseats 0))
+	 (len (length sortedseats)))
+    (while (and (< curr len) (= (elt sortedseats curr) (1+ last)))
+      (setq last (elt sortedseats curr))
+      (setq curr (1+ curr)))
+    (1- (elt sortedseats curr))))
+
 (defun findmaxid (seatlist)
-  "Given a list of seats '((2 3)(4 5)(6 7)...) will return the highest id."
+  "Given a list of seat IDs will return the highest id."
   (let ((curr 0)
 	(max 0)
 	(id 0)
-	(seat nil)
 	(numpasses (length seatlist)))
     (while (< curr numpasses)
-      (setq seat (elt seatlist curr))
-      (setq id (+ (* (elt seat 0) 8) (elt seat 1)))
+      (setq id (elt seatlist curr))
       (if (> id max) (setq max id))
       (setq curr (1+ curr)))
     max))
@@ -54,6 +76,14 @@
 (defun part1 (boardingpasses)
   (findmaxid (process boardingpasses)))
 
+;; This is the part2 wrapper
+(defun part2 (boardingpasses)
+  (findmissing (process boardingpasses)))
 
+;; Part 1 sample and actual
 (part1 (sample))
 (part1 (loaddaydata "./day05.input"))
+
+;; Part 2 sample and actual
+(part2 (sample))
+(part2 (loaddaydata "./day05.input"))
