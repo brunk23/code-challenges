ceae8b6bf03fbd441bbcb13c0918471aa8e801ed
Kevin Brunelle <brunellek@gmail.com>
2020-12-06 09:20:13 -0500

    Day 6 - Overcounts part 2
    There is a bug in part two where it overcounts the final amount.
    It is exactly 26 too high, because it includes the final array.

--- AdventOfCode2020/day06.el
+++ AdventOfCode2020/day06.el
@@ -35,21 +35,47 @@ b" "\n" nil))
       (if (> (aref counts curr) 0)
 	  (setq sum (1+ sum)))
       (setq curr (1+ curr)))
+    (message "%d" sum)
     sum))
 
 (defun countchars (str count)
-  "Will count the number of each characters in a string."
+  "Will note if a character is in a string."
   (let ((curr 0)
 	(chr 0)
 	(max (length str)))
     (while (< curr max)
       (setq chr (- (elt str curr) ?a))
-      (aset count chr (1+ (aref count chr)))
+      (aset count chr 1)
       (setq curr (1+ curr)))
     count))
 
-(defun countallgroup (inp)
-  "Will loop through the data file and sum all the groups."
+(defun andcounts (a b)
+  (let ((curr 0))
+    (while (< curr 26)
+      (aset a curr (logand (aref a curr) (aref b curr)))
+      (setq curr (1+ curr)))
+    a))
+
+(defun sumallgroupyes (inp)
+  "Will process the data and sum all the groups where everyone said yes."
+  (let ((counts (make-vector 26 1))
+	(curr 0)
+	(sum 0)
+	(max (length inp)))
+    (while (< curr max)
+      (if (< 0 (length (elt inp curr)))
+	  (setq counts
+		(andcounts counts
+			   (countchars (elt inp curr) (make-vector 26 0))))
+	(progn
+	  (setq sum (+ sum (sumcounts counts)))
+	  (setq counts (make-vector 26 1))))
+      (setq curr (1+ curr)))
+    (setq sum (+ sum (sumcounts counts)))
+    sum))
+
+(defun sumgroupyes (inp)
+  "Will process the data and sum all the groups where anyone said yes"
   (let ((counts (make-vector 26 0))
 	(curr 0)
 	(sum 0)
@@ -65,7 +91,15 @@ b" "\n" nil))
     sum))
 
 (defun part1 (data)
-  (countallgroup data))
+  (sumgroupyes data))
+
+(defun part2 (data)
+  (sumallgroupyes data))
 
 (part1 (sample))
 (part1 (loaddaydata "./day06.input"))
+
+(part2 (sample))
+(part2 (loaddaydata "./day06.input"))
+
+(- 3492 26)
